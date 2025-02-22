60308aa2bb1242f9940f5b0df9adac43eb4b51a0
Daniel Abayomi <dan234bayomi@gmail.com>
2025-02-22 19:03:28 +0100

    Added base player class

---
+++ player.js
@@ -0,0 +1,24 @@
+const { Hand, PlayStack } = require('./collection');
+const { Game } = require('./game');
+
+class Player {
+  /** @type {Hand} */
+  hand;
+  /** @type {PlayStack} */
+  playStack;
+
+  /**
+   * Creates a player
+   * @constructor
+   * @param {string} name - Player's name
+   * @param {Game} game - Game object
+   */
+  constructor (name, game) {
+    this.name = name;
+    this.game = game;
+  }
+
+  // TODO; addd pick function
+}
+
+module.exports = { Player };
