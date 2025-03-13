939926acb9fc94108f54f5f2ad8214edbfba8009
Daniel Abayomi <dan234bayomi@gmail.com>
2025-03-13 17:24:09 +0100

    Added allowable card object and validation in card initialization

--- card.js
+++ card.js
@@ -1,7 +1,27 @@
-/** @module Cards object modules */
+/**
+ * @fileoverview A file containing the card model and related functions
+ * and errors.
+ */
 
 /**
- * Whot card class
+ * A map of allowable card numbers based on shape.
+ * @type {Object.<string, Array.<number>}
+ */
+const ALLOWABLE_CARDS = {
+  square: [1, 2, 3, 5, 7, 10, 11, 13, 14],
+  circle: [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14],
+  triangle: [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14],
+  cross: [1, 2, 3, 5, 7, 10, 11, 13, 14],
+  star: [1, 2, 3, 4, 5, 7, 8],
+  whot: [20, 20, 20, 20, 20]
+};
+
+function name (params) {
+
+}
+
+/**
+ * Whot card model
  * @class
  */
 class Card {
@@ -12,7 +32,15 @@ class Card {
    * @param {number} number - The cards corresponding number
    */
   constructor (shape, number) {
+    shape = shape.toLowerCase();
+    if (!(Object.keys(ALLOWABLE_CARDS).includes(shape))) {
+      throw new Error(`Invalid shape '${shape}'`);
+    }
     this.shape = shape;
+    const validNumbers = ALLOWABLE_CARDS[shape];
+    if (!(validNumbers.includes(number))) {
+      throw new Error(`Invalid number '${number}'' for shape '${shape}'`);
+    }
     this.number = number;
   }
 }
